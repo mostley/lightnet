2339aa850fb2f0b2736630a08db4e93c05b27dfd
Sven Hecht <sven.hecht@citrix.com>
2016-02-13 21:39:11 +0100

    removed idea file from tracking

--- webinterface/index.js
+++ webinterface/index.js
@@ -22,5 +22,5 @@ var server = app.listen(config.port, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Lightnet Webinterface listening at http://%s:%s', host, port);
 });
